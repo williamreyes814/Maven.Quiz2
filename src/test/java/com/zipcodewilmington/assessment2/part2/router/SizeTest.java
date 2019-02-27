527a0457ff93ae0e6a220e9d4786d45257662213
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-02-27 18:36:03 -0500

    updated tests

--- src/test/java/com/zipcodewilmington/assessment2/part2/router/SizeTest.java
+++ src/test/java/com/zipcodewilmington/assessment2/part2/router/SizeTest.java
@@ -50,31 +50,4 @@ public class SizeTest {
         // then
         Assert.assertEquals(expectedSize, actualSize);
     }
-
-
-
-
-
-
-
-
-
-
-    class Pair<KeyType, ValueTyp> {
-        private KeyType key;
-        private ValueTyp value;
-
-        public Pair(KeyType key, ValueTyp value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public KeyType getKey() {
-            return key;
-        }
-
-        public ValueTyp getValue() {
-            return value;
-        }
-    }
 }
--- src/test/java/com/zipcodewilmington/assessment2/part2/router/ToStringTest.java
+++ src/test/java/com/zipcodewilmington/assessment2/part2/router/ToStringTest.java
@@ -1,6 +1,5 @@
 package com.zipcodewilmington.assessment2.part2.router;
 
-import com.zipcodewilmington.assessment2.part2.ListUtility;
 import com.zipcodewilmington.assessment2.part2.Router;
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,21 +7,57 @@ import org.junit.Test;
 public class ToStringTest {
     @Test
     public void test1() {
+        test(
+                new Pair<>("/users", "UserController"));
+    }
+
+    @Test
+    public void test2() {
+        test(
+                new Pair<>("/users", "UserController"),
+                new Pair<>("/students", "StudentController"),
+                new Pair<>("/instructors", "InstructorController"),
+                new Pair<>("/students", "StudentController"));
+    }
+
+
+    @Test
+    public void test3() {
+        test(
+                new Pair<>("/users", "UserController"),
+                new Pair<>("/instructors", "InstructorController"));
+    }
+
+
+    @Test
+    public void test4() {
+        test(
+                new Pair<>("/users", "UserController"),
+                new Pair<>("/instructors", "InstructorController"));
+    }
+
+    @Test
+    public void test(Pair<String, String>... pairs) {
         // given
+        StringBuilder expectedString = new StringBuilder();
         Router router = new Router();
-        router.add("/users", "UserController");
-        router.add("/students", "StudentController");
-        router.add("/instructor", "InstructorController");
-        String expectedString = "/instructor -> InstructorController\n"
-                + "/students -> StudentController\n"
-                + "/users -> UserController\n";
+        for (Pair<String, String> pair : pairs) {
+            String path = pair.getKey();
+            String controller = pair.getValue();
+
+            router.add(path, controller);
+            expectedString.append(path).append(controller).append('\n');
+        }
+
 
         // when
         String actualString = router.toString();
 
 
         // then
-        Assert.assertEquals(expectedString, actualString);
+        Assert.assertEquals(expectedString.toString(), actualString);
+
     }
+
 
 }
